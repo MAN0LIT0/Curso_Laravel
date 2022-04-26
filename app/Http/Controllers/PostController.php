4a64def1963e69fb1fdd7fc64e938787c030ab59
Marco Gondim <marcoantonioantonio949@gmail.com>
2022-04-26 16:01:28 -0300

    update e delete de dados

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(){
-        $posts = Post::get();
+        $posts = Post::latest()->paginate();
 
         return view('admin/posts/index', compact('posts'));
     }
@@ -22,7 +22,53 @@ class PostController extends Controller
 
        $post = Post::create($request->all());
 
-       return redirect()->route('posts.index');
+       return redirect()
+            ->route('posts.index')
+            ->with('message', 'Post Criado com sucesso!!!');
 
     }
+
+    public function show($id){
+       //$post = Post::where('id', $id)->first();
+       if(!$post = Post::find($id)){
+        return redirect()->route('posts.index');
+       }
+
+       return view('admin.posts.show', compact('post'));
+    }
+
+    public function destroy($id){
+
+        if(!$post = Post::find($id))
+            return redirect()->route('posts.index');
+
+        $post->delete();
+
+        return redirect()
+            ->route('posts.index')
+            ->with('message', 'Post Deletado com sucesso!!!');
+
+    }
+
+    public function edit($id){
+
+        if(!$post = Post::find($id)){
+         return redirect()->back();
+        }
+
+        return view('admin.posts.edit', compact('post'));
+     }
+
+     public function update(StoreUpdatePost $request, $id){
+
+        if(!$post = Post::find($id)){
+         return redirect()->back();
+        }
+
+        $post->update($request->all());
+
+        return redirect()
+            ->route('posts.index')
+            ->with('message', 'Post Atualizado com sucesso!!!');
+     }
 }
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,21 @@
+@if (session('message'))
+    <div>
+        {{ session('message') }}
+    </div>
+@endif
+
 <h1>Posts</h1>
 <a href="{{ route('posts.create') }}">Cria novo post</a>
 @foreach ($posts as $post)
-    <p>{{ $post->title }}</p>
+    <p>{{ $post->title }}
+       [
+        <a href="{{ route('posts.show', $post->id) }}">Ver detalhes</a>|
+        <a href="{{ route('posts.edit', $post->id) }}">Editar</a>
+
+       ]
+    </p>
 @endforeach
+
+<hr>
+
+{{ $posts->links() }}
